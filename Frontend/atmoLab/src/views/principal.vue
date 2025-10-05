1f2d958a5dc969270a0b2362e43d7f200fe33be1
D4N1EL19 <danineitor.gt.rs@gmail.com>
2025-10-05 01:33:18 -0700

    correcion de bg-principal

--- Frontend/atmoLab/src/views/principal.vue
+++ Frontend/atmoLab/src/views/principal.vue
@@ -38,7 +38,7 @@
       </div>
 
       <!-- 🔹 Celda 6: Abajo derecha -->
-      <div class="p-4 flex items-center justify-center  bg-white/10 w-full">
+      <div class="p-4 flex items-center justify-center">
         <calendario />
       </div>
     </div>
